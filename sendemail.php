ebc43276842331447514c16efeabc2f2a325255d
Audiofactory-admin <108276860+Audiofactory-admin@users.noreply.github.com>
2022-07-21 15:27:11 +0530

    I

--- sendemail.php
+++ sendemail.php
@@ -2,7 +2,7 @@
 	header('Content-type: application/json');
 	$status = array(
 		'type'=>'success',
-		'message'=>'Thank you for contact us. As early as possible  we will contact you '
+		'message'=>'Thank you for contact us. As early as possible  we will contact you'
 	);
 
     $name = @trim(stripslashes($_POST['name'])); 
